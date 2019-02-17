b6b1e14b6ef58a79feb0bb59b65dcb04f9d75a2b
Dale Weiler <weilercdale@gmail.com>
2019-02-17 18:11:30 -0500

    debug messages and asserts make multiple calls to stdio, protect them by a spin lock

--- src/rx/core/assert.cpp
+++ src/rx/core/assert.cpp
@@ -4,10 +4,20 @@
 
 #include <rx/core/assert.h>
 
+#include <rx/core/concurrency/spin_lock.h>
+#include <rx/core/concurrency/scope_lock.h>
+
+using rx::concurrency::scope_lock;
+using rx::concurrency::spin_lock;
+
+static spin_lock g_lock;
+
 [[noreturn]]
 void rx::assert_fail(const char* expression, const char* file,
   const char* function, int line, const char* message, ...)
 {
+  scope_lock<spin_lock> locked(g_lock);
+
   fprintf(stderr, "Assertion failed: %s (%s: %s: %d) \"",
     expression, file, function, line);
 
--- src/rx/core/debug.cpp
+++ src/rx/core/debug.cpp
@@ -3,9 +3,19 @@
 
 #include <rx/core/debug.h>
 
+#include <rx/core/concurrency/spin_lock.h>
+#include <rx/core/concurrency/scope_lock.h>
+
+using rx::concurrency::scope_lock;
+using rx::concurrency::spin_lock;
+
+static spin_lock g_lock;
+
 void rx::debug_message(const char *file, const char *function, int line,
   const char *message, ...)
 {
+  scope_lock<spin_lock> locked(g_lock);
+
   fprintf(stderr, "debug: ");
   va_list va;
   va_start(va, message);
