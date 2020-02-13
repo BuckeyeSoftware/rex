9a79f72478a3707521e7d46144ce73f8280faae0
Dale Weiler <weilercdale@gmail.com>
2020-02-13 02:49:07 -0500

    [core] avoid OS headers in concurrency headers

--- src/rx/core/concurrency/thread.h
+++ src/rx/core/concurrency/thread.h
@@ -1,23 +1,10 @@
 #ifndef RX_CORE_CONCURRENCY_THREAD_H
 #define RX_CORE_CONCURRENCY_THREAD_H
-#include "rx/core/config.h" // RX_PLATFORM_*
 #include "rx/core/function.h"
-
-#if defined(RX_PLATFORM_POSIX)
-#include <pthread.h> // pthread_t
-#elif defined(RX_PLATFORM_WINDOWS)
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#define VC_EXTRALEAN
-#include <windows.h> // HANDLE
-#undef interface
-#else
-#error "missing thread implementation"
-#endif
 
 namespace rx::concurrency {
 
-// NOTE: thread names must be static strings
+// Thread names must have static-storage which lives as long as the thread.
 struct thread
   : concepts::no_copy
 {
@@ -39,11 +26,8 @@ private:
 
     void join();
 
-#if defined(RX_PLATFORM_POSIX)
-    pthread_t m_thread;
-#elif defined(RX_PLATFORM_WINDOWS)
-    HANDLE m_thread;
-#endif
+    // Fixed-capacity storage for any OS thread type, adjust if necessary.
+    alignas(16) rx_byte m_thread[16];
 
     function<void(int)> m_function;
     bool m_joined;
