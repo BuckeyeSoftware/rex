50652b092c5d1e96858243a11c2523558528cb36
Dale Weiler <weilercdale@gmail.com>
2020-09-03 15:54:45 -0400

    [math] add line type

---
+++ src/rx/math/line.h
@@ -0,0 +1,14 @@
+#ifndef RX_MATH_LINE_H
+#define RX_MATH_LINE_H
+#include "rx/math/vec3.h"
+
+namespace Rx::Math {
+
+struct Line {
+  Vec3f a;
+  Vec3f b;
+};
+
+} // namespace Rx::Math
+
+#endif // RX_MATH_LINE_H
