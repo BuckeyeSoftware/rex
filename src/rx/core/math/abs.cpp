a03c54d2cfee8e30d02a315bcdc10440fb847786
Dale Weiler <weilercdale@gmail.com>
2020-12-27 20:57:04 -0500

    [core/math] consistency fix in abs

--- src/rx/core/math/abs.cpp
+++ src/rx/core/math/abs.cpp
@@ -5,13 +5,13 @@ namespace Rx::Math {
 
 Float64 abs(Float64 _x) {
   Shape u{_x};
-  u.as_u64 &= -1ull / 2;
+  u.as_u64 &= -1_u64 / 2;
   return u.as_f64;
 }
 
 Float32 abs(Float32 _x) {
   Shape u{_x};
-  u.as_u32 &= 0x7fffffff;
+  u.as_u32 &= -1_u32 / 2;
   return u.as_f32;
 }
 
