1419dff42281afa13479316ce8f9f01dbdecccb6
Dale Weiler <weilercdale@gmail.com>
2021-03-11 04:51:05 -0500

    [math] fix epsilon_compare

--- src/rx/math/compare.h
+++ src/rx/math/compare.h
@@ -1,12 +1,13 @@
 #ifndef RX_MATH_COMPARE_H
 #define RX_MATH_COMPARE_H
 #include "rx/core/math/abs.h" // abs
+#include "rx/core/math/constants.h" // EPSILON
 #include "rx/core/algorithm/max.h" // max
 
 namespace Rx::Math {
 
 inline bool epsilon_compare(Float32 _x, Float32 _y) {
-  return abs(_x - _y) <= EPSILON<Float32> * Algorithm::max(1.0f, _x, _y);
+  return abs(_x - _y) < EPSILON<Float32> * Algorithm::max(1.0f, _x, _y);
 }
 
 } // namespace Rx::Math
