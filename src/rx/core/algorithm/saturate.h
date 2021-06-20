1bcdf00bbc3f52b1b9998ff03237c1aeb4e5cf14
Dale Weiler <weilercdale@gmail.com>
2021-06-20 08:13:59 -0400

    fix comment

--- src/rx/core/algorithm/saturate.h
+++ src/rx/core/algorithm/saturate.h
@@ -8,8 +8,8 @@
 namespace Rx::Algorithm {
 
 /// Saturate floating point value to [0, 1] range.
-/// \param _value The value fo saturate
-/// \returns The saturated value
+/// \param _value The value to saturate.
+/// \returns The saturated value.
 RX_HINT_FORCE_INLINE constexpr auto saturate(Concepts::FloatingPoint auto _value) {
   using T = decltype(_value);
   return _value < T(0) ? T(0) : (_value > T(1) ? T(1) : _value);
