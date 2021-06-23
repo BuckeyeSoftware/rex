9418b06fcfbc56a50f3b27a80773f832d89b7015
Dale Weiler <weilercdale@gmail.com>
2021-06-23 14:26:51 -0400

    [core] nicely format assertions

--- src/rx/core/assert.cpp
+++ src/rx/core/assert.cpp
@@ -1,9 +1,5 @@
-#include <stdlib.h> // abort
-
-#include "rx/core/log.h" // RX_LOG, rx::log
-#include "rx/core/abort.h" // rx::abort
-
-RX_LOG("assert", logger);
+#include "rx/core/abort.h" // abort
+#include "rx/core/assert.h"
 
 namespace Rx {
 
@@ -11,12 +7,13 @@ namespace Rx {
 void assert_message(const char* _expression,
   const SourceLocation& _source_location, const char* _message, bool _truncated)
 {
-  // Write the assertion failure to the logger.
-  logger->error("Assertion failed: %s (%s:%d %s) \"%s\"%s",
-    _expression, _source_location.file(), _source_location.line(),
-    _source_location.function(), _message, _truncated ? "... [truncated]" : "");
-
-  abort(_truncated ? "%s... [truncated]" : "%s", _message);
+  abort("Assertion failed:\n"
+        "  Expression:  %s\n"
+        "  Location:    %s:%d\n"
+        "  Function:    %s\n"
+        "  Description: %s%s",
+        _expression, _source_location.file(), _source_location.line(),
+          _source_location.function(), _message, _truncated ? "... [truncated]" : "");
 }
 
 } // namespace Rx
